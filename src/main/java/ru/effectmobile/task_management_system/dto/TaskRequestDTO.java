0af94e417156a49397b8692ef99755d39ecc0c44
Azer Agazade <82523317+AgazadeAV@users.noreply.github.com>
2025-03-13 23:45:29 +0400

    created dto classes, ci pipline, additional changelog files and updated previous changes (#2)
    * created dto classes, ci pipline, additional changelog files and updated previous changes
    
    * fixed pipeline
    
    * deleted unuseful test
    
    ---------
    
    Co-authored-by: Azar Aghazada <a.aghazada@andersenlab.com>

---
+++ src/main/java/ru/effectmobile/task_management_system/dto/TaskRequestDTO.java
@@ -0,0 +1,22 @@
+package ru.effectmobile.task_management_system.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import ru.effectmobile.task_management_system.model.enums.TaskPriority;
+import ru.effectmobile.task_management_system.model.enums.TaskStatus;
+
+import java.util.UUID;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class TaskRequestDTO {
+    private String title;
+    private String description;
+    private TaskStatus status;
+    private TaskPriority priority;
+    private UUID assigneeId;
+}
