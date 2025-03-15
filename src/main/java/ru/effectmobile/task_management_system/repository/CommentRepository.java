e4110fd6184913b13fa896eaf9e16260a3158e52
Azer Agazade <82523317+AgazadeAV@users.noreply.github.com>
2025-03-16 00:46:11 +0400

    Update CommentRepository.java

--- src/main/java/ru/effectmobile/task_management_system/repository/CommentRepository.java
+++ src/main/java/ru/effectmobile/task_management_system/repository/CommentRepository.java
@@ -3,10 +3,12 @@ package ru.effectmobile.task_management_system.repository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import ru.effectmobile.task_management_system.model.entity.Comment;
 
 import java.util.UUID;
 
+@Repository
 public interface CommentRepository extends JpaRepository<Comment, UUID> {
 
     Page<Comment> findByTaskId(UUID taskId, Pageable pageable);
